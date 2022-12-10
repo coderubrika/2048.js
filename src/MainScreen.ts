179af925d65f4cb75b0a54c358240840d34fda3e
Anton Samartsev <coderubrika@gmail.com>
2022-12-11 00:37:26 +0300

    first version of view model

--- src/MainScreen.ts
+++ src/MainScreen.ts
@@ -10,7 +10,7 @@ export class MainScreen {
   private readonly enableAIButton: HTMLButtonElement
   private readonly disableAIButton: HTMLButtonElement
   private readonly aiPowerInput: HTMLInputElement
-  private readonly aiSpeedInput: HTMLInputElement
+  private readonly aiStepRateInput: HTMLInputElement
   private readonly saveProgressButton: HTMLButtonElement
   private readonly loadProgressButton: HTMLButtonElement
   private readonly field: HTMLElement
@@ -19,9 +19,13 @@ export class MainScreen {
 
   public readonly OnPlay: Observable<MouseEvent>
   public readonly OnReset: Observable<MouseEvent>
-  public readonly OnEnableAI: Subject<boolean>
-  public readonly Scope: RxProperty<number>
-  public readonly Resolution: RxProperty<number>
+  public readonly OnEnableAI = new Subject<boolean>()
+  public readonly Scope = new RxProperty<number>(0)
+  public readonly Resolution = new RxProperty<number>(4)
+  public readonly AIPower = new RxProperty<number>(1)
+  public readonly AIStepRate = new RxProperty<number>(500)
+  public readonly OnSave = new Subject()
+  public readonly OnLoad = new Subject()
 
   constructor(gameManager) {
     this.screen = query('.screen.main')
@@ -34,7 +38,7 @@ export class MainScreen {
     this.enableAIButton = query('button.enable-ai', this.screen)
     this.disableAIButton = query('button.disable-ai', this.screen)
     this.aiPowerInput = query('input.ai-power', this.screen)
-    this.aiSpeedInput = query('input.ai-speed', this.screen)
+    this.aiStepRateInput = query('input.ai-speed', this.screen)
 
     this.saveProgressButton = query('button.save-progress', this.screen)
     this.loadProgressButton = query('button.load-progress', this.screen)
@@ -44,19 +48,26 @@ export class MainScreen {
     this.OnPlay = fromEvent<MouseEvent>(this.playButton, 'click')
     this.OnReset = fromEvent<MouseEvent>(this.resetButton, 'click')
 
-    this.OnEnableAI = new Subject<boolean>()
-
     fromEvent(this.enableAIButton, 'click').subscribe(() => {
       if (this.isEnabledAI)
         return
       this.isEnabledAI = true
       this.OnEnableAI.next(true)
     })
+
     fromEvent(this.disableAIButton, 'click').subscribe(() => {
       if (!this.isEnabledAI)
         return
       this.isEnabledAI = false
       this.OnEnableAI.next(false)
+    })
+
+    fromEvent(this.saveProgressButton, 'click').subscribe(() => {
+      this.OnSave.next(undefined)
+    })
+
+    fromEvent(this.loadProgressButton, 'click').subscribe(() => {
+      this.OnLoad.next(undefined)
     })
 
     this.Scope.subscribe(scope => {
@@ -67,11 +78,35 @@ export class MainScreen {
       const target = event.target as HTMLInputElement
       const candidate = Number(target.value)
       
-      if (Number.isNaN(candidate)) {
+      if (Number.isNaN(candidate) || !Number.isInteger(candidate) || candidate < 3) {
         target.value = this.Resolution.Value.toString()
       }
       else {
         this.Resolution.Value = candidate
+      }
+    })
+
+    fromEvent<KeyboardEvent>(this.aiPowerInput, 'keyup').subscribe(event => {
+      const target = event.target as HTMLInputElement
+      const candidate = Number(target.value)
+      
+      if (Number.isNaN(candidate) || !Number.isInteger(candidate) || candidate < 3) {
+        target.value = this.AIPower.Value.toString()
+      }
+      else {
+        this.AIPower.Value = candidate
+      }
+    })
+
+    fromEvent<KeyboardEvent>(this.aiStepRateInput, 'keyup').subscribe(event => {
+      const target = event.target as HTMLInputElement
+      const candidate = Number(target.value)
+      
+      if (Number.isNaN(candidate) || !Number.isInteger(candidate) || candidate < 3) {
+        target.value = this.AIStepRate.Value.toString()
+      }
+      else {
+        this.AIStepRate.Value = candidate
       }
     })
   }
